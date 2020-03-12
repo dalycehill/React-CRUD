c2a73f9625b025f61734912d91a8ca886dd27175
Dalyce Hill <50115499+dalycehill@users.noreply.github.com>
2020-03-12 09:15:43 -0300

    Add files via upload

---
+++ server/routes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const CategoryController = require('./controllers/CategoryController');
+const ItemController = require('./controllers/ItemController');
+const router = express.Router();
+
+//app routes
+router.get('/categories', CategoryController.index);
+router.post('/categories', CategoryController.store);
+router.patch('/categories/:category', CategoryController.update);
+router.delete('/categories/:category', CategoryController.destroy);
+
+router.get('/items', ItemController.index);
+router.post('/items', ItemController.store);
+router.patch('/items/:item', ItemController.update);
+router.delete('/items/:item', ItemController.destroy);
+
+module.exports = router;
